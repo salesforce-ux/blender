e0e912341567869bd6847dbf730ebe5184e6b010
Dennis Xu <dennis.xu@salesforce.com>
2014-08-15 12:46:11 -0700

    fully functional and reformatted config.json

--- test/test.js
+++ test/test.js
@@ -6,41 +6,6 @@ var path   		= require('path');
 var blender		= require('./../lib/blender.js');
 
 
-var types = [
-    {type: 'utility',   suffix: '',         oneX: 18, inputWidth: 64,  inputHeight: 64,  src: './test/icons/utility'},
-    {type: 'doctype',   suffix: '',         oneX: 18, inputWidth: 56,  inputHeight: 64,  src: './test/icons/doctype'},
-    {type: 'action',    suffix: '',         oneX: 18, inputWidth: 36,  inputHeight: 36,  src: './test/icons/actions'},
-    {type: 'custom',    suffix: '_anchor',  oneX: 48, inputWidth: 120, inputHeight: 120, src: './test/icons/custom'},
-    {type: 'standard',  suffix: '_anchor',  oneX: 48, inputWidth: 120, inputHeight: 120, src: './test/icons/standard/svg'},
-    {type: 'custom',    suffix: '_list',    oneX: 32, inputWidth: 120, inputHeight: 120, src: './test/icons/custom'},
-    {type: 'standard',  suffix: '_list',    oneX: 32, inputWidth: 120, inputHeight: 120, src: './test/icons/standard/svg'}
-];
-
-var androidSizes = [
-  {sizeLabel: '',   sizeDir : 'mdpi',   scale: 1,    ignoreSuffix: false},
-  {sizeLabel: '',   sizeDir : 'hdpi',   scale: 1.5,  ignoreSuffix: false},
-  {sizeLabel: '',   sizeDir : 'xhdpi',  scale: 2,    ignoreSuffix: false},
-  {sizeLabel: '',   sizeDir : 'xxdpi',  scale: 3,    ignoreSuffix: false}
-];
-
-var xSizes = [
-  {sizeLabel: '',    sizeDir : '',  scale: 1, ignoreSuffix: false},
-  {sizeLabel: '@2x', sizeDir : '',  scale: 2, ignoreSuffix: false}
-];
-
-var apiSizes = [
-  {sizeLabel: '_60',  sizeDir : '', scale: 1, size: 60,  ignoreSuffix: true},
-  {sizeLabel: '_120', sizeDir : '', scale: 1, size: 120, ignoreSuffix: true}
-];
-
-var formats = [
-  {sizes: androidSizes, name: 'android'},
-  {sizes: xSizes,       name: 'ios'}
-];
-
-var config = {formats: formats, types: types}
-
-
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000000;
 var i = 4;
 describe('blender', function() {
@@ -50,9 +15,12 @@ describe('blender', function() {
 	});
 
 	it("should create the appropriate folder structure", function(done) {
-		blender('./test/output', config, function() {
+		var config = fs.readFileSync('./config.json');
+		blender('./test/output', JSON.parse(config), function() {
+			console.log('complete');
 			i--;
 			if (i === 0) {
+				console.log('go')
 				done();
 			}
 		});
