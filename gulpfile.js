444a9d60a6ece30d242dba1c483d857db2eec867
Dennis Xu <dennis.xu@salesforce.com>
2014-08-18 11:04:13 -0700

    merged upstream

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,26 @@
 var gulp		= require('gulp');
 var gutil   = require('gulp-util');
-var jasmine = require('gulp-jasmine');
-
+var mocha		= require('gulp-mocha');
+var rimraf 	= require('rimraf');
 
 
 gulp.task('tdd', function() {
 	console.log('starting test...');
 	gulp.start('test');
 
-	gulp.watch(['lib/**'])
+	gulp.watch(['lib/**', 'test/testConfig.json'])
 		.on('change', function(file) {
+			console.log(file.path, 'changed');
 			gulp.start('test');
 		});
 });
 
-gulp.task('test', function() {
+gulp.task('clean-test', function(done) {
+	return rimraf('test/output', done)
+});
+
+gulp.task('test', ['clean-test'], function() {
 	return gulp.src(['test/*.js'])
-		.pipe(jasmine({ verbose: true }))
+		.pipe(mocha({ reporter: 'spec' }))
 		.on('error', gutil.log);
 });
